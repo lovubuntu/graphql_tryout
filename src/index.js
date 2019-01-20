ee5f15d2be3de123b29a420b044b1787e68ae076
Prabu <meetprabu88@gmail.com>
2019-01-20 15:17:23 +0530

    Uses extracted resolvers and updated schema for authentication and links creation

--- src/index.js
+++ src/index.js
@@ -1,47 +1,16 @@
 const {GraphQLServer} = require('graphql-yoga');
 const {prisma} = require('./generated/prisma-client');
-
-const resolvers = {
-	Query: {
-		info: () => 'The information u r looking for',
-		staticText: () => 'Static Text',
-		feed: (root, args, context) => context.prisma.links()
-	},
-
-	Link: {
-		id: (parent) => parent.id,
-		description: (parent) => parent.description,
-		url: (parent) => parent.url
-	},
-
-	Mutation: {
-		post: (parent, args, context) => {
-			const link = {
-				description: args.description,
-				url: args.url
-			};
-			return context.prisma.createLink(link);
-		},
-
-		update: (parent, args) => {
-			const link = links.find(l => l.id === args.id);
-			link.description = args.description;
-			link.url = args.url;
-			return link;
-		},
-
-		delete: (parent, args) => {
-			const linkIndex = links.findIndex(l => l.id === args.id);
-			console.log(linkIndex, args.id, parent, 'deleting', links);
-			return linkIndex === -1 ? {} : links.splice(linkIndex, 1)[0];
-		}
-	}
-}
+const resolvers = require('./resolvers');
 
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
 	resolvers,
-	context: {prisma}
+	context: request => {
+		return {
+			prisma,
+			...request
+		};
+	}
 });
 
 server.start(() => console.log('Here I come to world of graphql. Head to http://localhost:4000... Now!'));
